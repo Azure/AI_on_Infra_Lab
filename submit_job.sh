68314de80cde2f7c91a88eda88f994105a00f6f7
Jennifer Hajduk <jhajduk@microsoft.com>
2024-07-09 17:01:57 -0700

    added parameter to pass in

--- submit_job.sh
+++ submit_job.sh
@@ -11,10 +11,6 @@
 #Pre-requisites
 pip install numpy==1.22.4 torch transformers datasets
 
-# Load modules or activate environment if needed
-module load python/3.8
-module load cuda/12.4
-
 # Run the Python script
 python3 distilbert-base-uncased.py --sample_text "This is a great movie with a thrilling plot"
 
